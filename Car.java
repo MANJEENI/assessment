5aa847f3d93c4121bcb36d87832abf2ccbd571c4
MANJEENI <mansitybsc@gmail.com>
2023-04-27 17:16:47 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Car {
+	private void applyBreak() {
+		
+		System.out.println("apply break");
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void soundHorn() {
+		
+		System.out.println("sound horn");
+		// TODO Auto-generated method stub
+
+	}
+	
+void Brand() {
+	System.out.println("kia");
+		// TODO Auto-generated method stub
+
+	}
+public static void main(String[] args) {
+	
+	Car out= new Car();
+	out.applyBreak();
+	out.Brand();
+	out.soundHorn();
+	System.out.println("welcome to hell");
+}
+}
